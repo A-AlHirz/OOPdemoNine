62995364b52c66ff71ae76d983c6755a08a79c69
A-AlHirz <abdherz@hotmail.com>
2024-12-25 11:29:06 +0300

    Add project files.

---
+++ Demonine/Models/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demonine.Models;
+
+namespace Demonine.Models
+{
+    public class Employee
+    {
+        private string emailAddress;
+        //Prop
+        public string Emailaddress
+        {
+            get { return emailAddress; }
+            set
+            {
+                if (value.Contains("@"))
+                {
+                    emailAddress = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email Address");
+                }
+            }
+        }
+        public void GetEmailAddres()
+        {
+            Console.WriteLine("My Email is " + emailAddress);
+        }
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+
+        //Object List of Employee
+        List<Employee> employees = new List<Employee>();
+
+
+        //AddEmployee
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        //GetEmployee
+        public void GetEmployess()
+        {
+            foreach(var emp in employees)
+            {
+                Console.WriteLine("ID: "+emp.Id+"\n Name: "+emp.Name+"\n Email: "+emp.Email+" \n Age: "+emp.Age);
+            }
+        }
+
+
+
+    }
+}
+
+#region anotherMethod
+//public void AddEmployee(int id , string name, string email, int age)
+//{
+
+//employees.Add(new Employee { Id=id, Name = name, Email = email, Age = age});
+
+//}
+
+#endregion
---
+++ Demonine/Program.cs
@@ -0,0 +1,49 @@
+﻿//object class
+#region Demo
+using Demonine.Models;
+
+Demo d1 = new Demo();
+Console.WriteLine("Enter your name: ");
+d1.SetName(Console.ReadLine());
+
+Console.WriteLine("Welcome " + d1.GetName());
+#endregion
+
+#region VaildEmail
+
+Employee emp = new Employee();
+Console.WriteLine("Enter Email Address: ");
+emp.Emailaddress = Console.ReadLine();
+
+#endregion
+
+#region Employee
+//object employee class
+Console.WriteLine("Enter number of employees: ");
+int count=count=Convert.ToInt32(Console.ReadLine());
+Employee emp = new Employee();
+
+for (int i = 1; i <= count; i++)
+{
+    Console.WriteLine("Enter Employee ID: " + i);
+    int EmployeeNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter Employee Name: " + i);
+    string EmployeeName = Console.ReadLine();
+
+    Console.WriteLine("Enter Employee Email: " + i);
+    string EmailAddress = Console.ReadLine();
+
+    Console.WriteLine("Enter Employee Age: " + i);
+    int EmployeeAge = Convert.ToInt32(Console.ReadLine());
+
+    //Add New Employee
+   emp= new Employee { Id = EmployeeNumber, Name = EmployeeName, Email = EmailAddress, Age = EmployeeAge };
+
+    emp.AddEmployee(emp);
+
+}
+Console.WriteLine("Employee Id  Employee Name  Email  Age ");
+emp.GetEmployess();
+
+#endregion
