953149ea94bed3902ea276a6757b180eb065d724
A-AlHirz <abdherz@hotmail.com>
2024-12-25 14:02:55 +0300

    updated

---
+++ Demonine/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demonine.Models
+{
+    public class Category : Course
+    {
+        public int CategoryId { get; set; }
+
+        public override void CourseDetails()
+        {
+            Console.WriteLine("Category Class: Developement" + " , C# Course ");
+        }
+
+    }
+}
---
+++ Demonine/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demonine.Models
+{
+    public class Course
+    {
+        //Overload : Same name, Dif par or Datatype
+       
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+
+        public virtual void CourseDetails()
+        {
+            Console.WriteLine("Course Class: Default Value");
+        }
+    }
+}
